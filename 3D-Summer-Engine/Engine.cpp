e1bc57a51ded431288c6c6d43fcb8ee3a3f58d65
Tobias <tobbe12a@hotmail.se>
2022-12-05 10:58:22 +0100

    Uncommented fps constraint

--- 3D-Summer-Engine/Engine.cpp
+++ 3D-Summer-Engine/Engine.cpp
@@ -98,7 +98,7 @@ void Engine::Run() {
 		g_lastTime = glfwGetTime();
 		frames++;
 
-		//sleepTime = 1000.0f / 144.0f - g_deltaTime * 1000.0f;
+		sleepTime = 1000.0f / 144.0f - g_deltaTime * 1000.0f;
 		std::string title = "FPS: " + std::to_string(fps) + " dT: " + std::to_string(g_deltaTime*1000.0f) + "ms TPF: " + std::to_string(TPF) + "ms";
 		if (currentTime - g_lastTime2 >= 1.f)
 		{
@@ -162,7 +162,7 @@ void Engine::Run() {
 		
 		if (sleepTime > 0)
 		{
-			//std::this_thread::sleep_for(std::chrono::milliseconds((long)(sleepTime)));
+			std::this_thread::sleep_for(std::chrono::milliseconds((long)(sleepTime)));
 		}
 
 	}
