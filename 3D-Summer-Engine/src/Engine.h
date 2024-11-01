21af6ac98f68229170e6c05dae6aa50abb20ff4d
Tobias Andersson <tobbe12a@hotmail.se>
2024-11-01 04:07:02 +0100

    changed resolution

--- 3D-Summer-Engine/src/Engine.h
+++ 3D-Summer-Engine/src/Engine.h
@@ -31,10 +31,10 @@ private:
 	Camera m_camera;
 
 	//Window Properties
-	//const unsigned int c_WIDTH								 = 2560;
-	//const unsigned int c_HEIGHT								 = 1440;
-	const unsigned int c_WIDTH								 = 1920;
-	const unsigned int c_HEIGHT								 = 1080;
+	const unsigned int c_WIDTH								 = 2560;
+	const unsigned int c_HEIGHT								 = 1440;
+	//const unsigned int c_WIDTH								 = 1920;
+	//const unsigned int c_HEIGHT								 = 1080;
 	const char* c_WINDOW_NAME								 = "Summer Engine";
 	const float c_DEFAULT_CLEAR_COLOR[4]					 = { 1.0f, 0.0f, 0.0f, 1.0f };
 	const float c_CLEAR_COLOR[4]							 = { 0.28f, 0.41f, 0.61f, 1.0f };
